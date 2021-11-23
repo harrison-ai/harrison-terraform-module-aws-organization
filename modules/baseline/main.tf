b2601bf00320a98553f72920590f30e429ce2b16
Carl Hattingh <carl.hattingh@harrison.ai>
2021-11-23 21:41:15 +1100

    chore: add fixme note MOD-87

--- modules/baseline/main.tf
+++ modules/baseline/main.tf
@@ -86,7 +86,8 @@ data "external" "delete_default_vpc" {
 module "subnetting" {
   count = local.account.create_vpc ? 1 : 0
 
-  source = "../cidr"
+  #  fixme once merged
+  source = "git@github.com:harrison-ai/harrison-terraform-module-vpc-cidrs.git?ref=feature/MOD-162-initial-config"
 
   cidr          = local.account.vpc_cidr
   subnet_prefix = 24
