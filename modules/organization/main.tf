c5e4b91b85d1470ee5ea2e4819fde72a95084847
Carl Hattingh <44431287+schattingh@users.noreply.github.com>
2022-11-15 21:15:09 +1100

    Tag member accounts (#17)

--- modules/organization/main.tf
+++ modules/organization/main.tf
@@ -31,6 +31,7 @@ resource "aws_organizations_account" "this" {
   iam_user_access_to_billing = each.value.iam_user_access_to_billing
   parent_id                  = each.value.parent.uuid != null ? [for ou in local.org_units : ou.id if ou.uuid == each.value.parent.uuid][0] : aws_organizations_organization.org.roots[0].id
   close_on_deletion          = each.value.close_on_deletion
+  tags                       = try(each.value.tags, {})
 
   depends_on = [
     aws_organizations_organization.org
