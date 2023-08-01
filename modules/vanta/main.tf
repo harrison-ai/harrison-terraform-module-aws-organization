5a3cd43a6b15cef9858afb856b5ebeb0200528fe
Daniel Carrion <daniel.carrion@harrison.ai>
2023-08-01 14:38:21 +1000

    wip

--- modules/vanta/main.tf
+++ modules/vanta/main.tf
@@ -1,19 +1,13 @@
 resource "aws_iam_role" "vanta_auditor" {
   name               = "vanta-auditor"
   assume_role_policy = data.aws_iam_policy_document.vanta_assume_role.json
+  managed_policy_arns = [
+    aws_iam_policy.vanta_access.arn,
+    "arn:aws:iam::aws:policy/SecurityAudit",
+    ]
 }
 
 resource "aws_iam_policy" "vanta_access" {
   name   = "vanta-access"
   policy = data.aws_iam_policy_document.vanta_access.json
-}
-
-resource "aws_iam_role_policy_attachment" "vanta_access" {
-  role       = aws_iam_role.vanta_auditor.name
-  policy_arn = aws_iam_policy.vanta_access.arn
-}
-
-resource "aws_iam_role_policy_attachment" "vanta_security_audit" {
-  role       = aws_iam_role.vanta_auditor.name
-  policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
 }
